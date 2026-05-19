a614e30971ca263bfd4b1ee5f60100cf98ce015c
IvicaSkrobo <IvicaSkrobo@users.noreply.github.com>
2026-05-19 16:53:11 +0200

    Link app bug reports directly

--- src/lib/externalLinks.ts
+++ src/lib/externalLinks.ts
@@ -1,3 +1,4 @@
 export const WEBSITE_URL = 'https://ivicaskrobo.github.io/Bili-Mushroom/';
-export const BUG_REPORT_URL = `${WEBSITE_URL}#bug-report`;
+export const BUG_REPORT_URL =
+  'https://github.com/IvicaSkrobo/Bili-Mushroom/issues/new?template=bug_report.yml&labels=bug';
 export const DONATE_URL = `${WEBSITE_URL}#support`;
